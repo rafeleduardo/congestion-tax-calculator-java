d988f04a414981c957d1667a361819d8a00aee57
Rafael Eduardo Parra Peña <rafeleduardo@gmail.com>
2024-06-06 09:59:57 -0500

    First commit

---
+++ src/main/java/org/rafeleduardo/congestiontaxcalculator/CongestionTaxCalculatorJavaApplication.java
@@ -0,0 +1,13 @@
+package org.rafeleduardo.congestiontaxcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongestionTaxCalculatorJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongestionTaxCalculatorJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rafeleduardo/congestiontaxcalculator/CongestionTaxCalculatorJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rafeleduardo.congestiontaxcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongestionTaxCalculatorJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
